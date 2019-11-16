c21224696f5f6778152f4b1bea30bb77fd3cd757
weili <weili@sensorsdata.cn>
2019-11-16 11:50:51 +0800

    change mget and mset

--- main.go
+++ main.go
@@ -12,9 +12,9 @@ import (
 	"time"
 )
 
+var msetNum int = 200
+var mgetNum int = 20
 func main() {
-	//msetNum := 200
-	mgetNum := 2000
 	cfgPath, _ := filepath.Abs("./config.json")
 	rawCfg, err := ioutil.ReadFile(cfgPath)
 	if err != nil {
@@ -34,38 +34,56 @@ func main() {
 	if e1 != nil {
 		fmt.Println("err 发生", e1)
 	}
+
+	mset(tb)
+	//mget(tb)
+
+}
+
+
+func mget(tb pegasus.TableConnector){
 	sortedKeys := make([][]byte, mgetNum)
 	total := int64(0)
-	for j := 0; j < 100000; j++ {
+	for j := 0; j < 10000000; j++ {
 		for i := 0; i < mgetNum; i++ {
 			rand1 := rand.New(rand.NewSource(time.Now().UnixNano()))
-			k := rand1.Intn(240000000)
+			k := rand1.Intn(6)
 			s := strconv.Itoa(k)
+			//fmt.Println("sss", s)
 			sortedKeys[i] = []byte(s)
 		}
 		start := time.Now().UnixNano()
-		_, _, _ = tb.MultiGet(context.Background(), []byte("0"), sortedKeys)
+		values, _, _ := tb.MultiGet(context.Background(), []byte("0"), sortedKeys)
+		vvv := ""
+		kkk := ""
+		for _, vv := range values {
+			vvv = string(vv.Value)
+			kkk = string(vv.SortKey)
+		}
 		end := time.Now().UnixNano()
 		delta := end - start
 		total = total + delta/1000000
-		fmt.Println("第 %s 次 mget， 时间= %s ns", j, delta, total)
+		fmt.Println("第 %s 次 mget， 时间= %s ns", j, delta, total, vvv, kkk)
 	}
+}
 
-	/*	for i := 0; i < 1000; i++ {
+func mset(tb pegasus.TableConnector) {
+	total := int64(0)
+	for i := 1000000; i < 1500000; i++ {
 		sortedKeys := make([][]byte, msetNum)
 		values := make([][]byte, msetNum)
 		for j := 0; j < msetNum; j++ {
 			var k int
-			k = i * msetNum + j
+			k = i*msetNum + j
 			s := strconv.Itoa(k)
 			sortedKeys[j] = []byte(s)
 			values[j] = []byte(s)
 		}
 		start := time.Now().UnixNano()
-		tb.MultiSet(context.Background(), []byte("0"), sortedKeys, values)
+		_ = tb.MultiSet(context.Background(), []byte("1"), sortedKeys, values)
 		end := time.Now().UnixNano()
 		delta := end - start
 		total = total + delta/1000000
-		fmt.Println("第 %s 次 mset， 时间= %s ns", i, delta, total)
-	}*/
+		fmt.Println(fmt.Sprintf("第 %d 次 mset， 时间= %d ns, 平均时间= %d ns", i, delta, total/int64(i-1000000+1)))
+	}
 }
